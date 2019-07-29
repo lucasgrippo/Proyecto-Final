a1d68c7907e309ecf2e606e331ae592d70e70c6c
nacho <moyaignacioj@gmail.com>
2019-07-29 18:21:55 -0300

    agregue flowgraph sin GUI y script grabador automatico

--- run_decod.sh
+++ run_decod.sh
@@ -1,4 +1,4 @@
 source /home/prefix/default/setup_env.sh
 CURRENTDATE=`date +"%Y-%m-%d-%T"`
-python top_block.py -i 1.wav -o ${CURRENTDATE}
-#met_decoder -q -c out_soft.s output_pic
+python top_block.py -i 2.wav -o ${CURRENTDATE}
+met_decoder -q -c ${CURRENTDATE}.s output_pic
